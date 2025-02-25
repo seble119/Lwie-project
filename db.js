bc54a5937fbe71e91043782ea35a7305a0103e42
seble119 <seble11994@gmail.com>
2025-02-25 22:53:29 +0400

    2nd

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "user_registration",
-    password: "12212729",
-    port: 5432,
+const { Pool } = require("pg");
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL, // Use an environment variable for security
+    ssl: { rejectUnauthorized: false } // Required for Neon database
 });
 
 module.exports = pool;
